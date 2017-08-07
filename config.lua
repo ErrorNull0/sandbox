ab1b811fab08ebf95c9d2a5f7c9fc7b9875043e8
ErrorNull <jasonlim@onsiteone.com>
2017-08-07 04:14:50 -0700

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,6 @@
+-- set 'false' to not give player startup coins
+villagers.startup_coins = true
+villagers.stock_min_items = 40
+villagers.stock_max_items = 70
+villagers.stock_min_coins = 700
+villagers.stock_max_coins = 990
